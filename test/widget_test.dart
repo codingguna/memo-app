ee1d9aac23094a2ffed877ea14d7433a61412aa6
gs400pass <gs400pass@gmail.com>
2025-06-17 11:51:51 +0000

    update files

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:myapp/main.dart';
+import 'package:hospital_management/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
